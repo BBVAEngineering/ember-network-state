95ae79b88d2042bfda02af95aac87de02e765bf0
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2018-07-10 15:46:08 +0200

    [fix] remove lts from tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,14 +4,6 @@
 module.exports = {
 	scenarios: [
 		{
-			name: 'ember-lts-2.16',
-			npm: {
-				devDependencies: {
-					'ember-source': '~2.16.0'
-				}
-			}
-		},
-		{
 			name: 'ember-release',
 			bower: {
 				dependencies: {
