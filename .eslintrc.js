e1092f5a5840164c068496856638cffa5ee59039
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2019-01-02 17:12:40 +0100

    chore(files): update common files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 'use strict';
 
 module.exports = {
@@ -7,8 +6,44 @@ module.exports = {
 		ecmaVersion: 2017,
 		sourceType: 'module'
 	},
-	extends: 'eslint-config-bbva',
+	plugins: [
+		'ember'
+	],
+	extends: [
+		'plugin:ember/recommended',
+		'eslint-config-bbva'
+	],
 	env: {
 		browser: true
-	}
+	},
+	overrides: [{
+		files: [
+			'.huskyrc.js',
+			'.commitlintrc.js',
+			'.eslintrc.js',
+			'.template-lintrc.js',
+			'ember-cli-build.js',
+			'index.js',
+			'testem.js',
+			'blueprints/*/index.js',
+			'config/**/*.js',
+			'tests/dummy/config/**/*.js'
+		],
+		excludedFiles: [
+			'addon/**',
+			'addon-test-support/**',
+			'app/**',
+			'tests/dummy/app/**'
+		],
+		parserOptions: {
+			sourceType: 'script',
+			ecmaVersion: 2015
+		},
+		env: {
+			browser: false,
+			node: true
+		},
+		plugins: ['node'],
+		rules: require('eslint-plugin-node').configs.recommended.rules
+	}]
 };
--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,7 +5,7 @@ This doc outlines the details of collaborating on this Ember addon.
 ## Installation
 
 * `git clone <repository-url>` this repository
-* `cd ember-network-state`
+* `cd <repository-name>`
 * `npm install`
 
 ## Eslint
