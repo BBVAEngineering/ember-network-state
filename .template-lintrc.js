f936a91a82d9df46535500caab3c18e4fd150954
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2020-09-15 15:04:37 +0200

    chore(files): update common files

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+	extends: 'octane',
+	rules: {
+		'no-bare-strings': true,
+		'block-indentation': 'tab',
+		'no-html-comments': true,
+		'no-nested-interactive': true,
+		'self-closing-void-elements': true,
+		'no-triple-curlies': true,
+		'deprecated-each-syntax': true,
+		'link-rel-noopener': true,
+		'no-invalid-interactive': true,
+		'require-valid-alt-text': true,
+		'style-concatenation': true,
+		'deprecated-inline-view-helper': true,
+		'no-unused-block-params': true,
+		'inline-link-to': true,
+		'no-inline-styles': true,
+		'simple-unless': false
+	},
+	ignore: [
+		'tests/dummy/**'
+	]
 };
--- testem.js
+++ testem.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-/* eslint-disable no-process-env */
 'use strict';
 
-||||||| parent of b5b0216... v3.7.1...v3.21.2
-=======
-'use strict';
-
->>>>>>> b5b0216... v3.7.1...v3.21.2
 module.exports = {
-<<<<<<< HEAD
 	test_page: 'tests/index.html?hidepassed',
 	disable_watching: true,
 	launch_in_ci: [
@@ -19,60 +11,14 @@ module.exports = {
 	],
 	browser_args: {
 		Chrome: [
-			process.env.TRAVIS ? '--no-sandbox' : null,
-			'--disable-gpu',
+			// --no-sandbox is needed when running Chrome inside a container
+			process.env.CI ? '--no-sandbox' : null,
 			'--headless',
+			'--disable-dev-shm-usage',
+			'--disable-software-rasterizer',
+			'--mute-audio',
 			'--remote-debugging-port=0',
 			'--window-size=1440,900'
 		].filter(Boolean)
 	}
-||||||| parent of b5b0216... v3.7.1...v3.21.2
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ].filter(Boolean)
-    }
-  }
-=======
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_start_timeout: 120,
-  browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ].filter(Boolean)
-    }
-  }
->>>>>>> b5b0216... v3.7.1...v3.21.2
 };
